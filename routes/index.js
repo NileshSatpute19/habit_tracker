509bf0160d847935ff3cad525711562dad8151c3
NileshSatpute19 <satputenilesh2000@gmail.com>
2023-12-17 00:43:28 +0530

    bugFix: solve the routes issue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+// importing express
 const express = require("express");
+
+// creating new router variable
 const router = express.Router();
-const { home, createHabit } = require("../controller/homeController");
 
-router.get("/", home);
-router.post("/addhabit", createHabit);
-router.use("/listhabits", require("./myHabits"));
+// getting home controller for the router
+const homeController = require("../controller/homeController");
 
+// calling controller for the home page
+router.get("/", homeController.home);
+
+// route to creating a new habit
+router.post("/create-habit", homeController.createHabit);
+
+// route for details page
+router.use("/my-habits", require("./myHabits"));
+
+// exporting the router for outside use
 module.exports = router;
